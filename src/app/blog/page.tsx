f1440d53dc1c5d74ab2e9e162c8c5cc0faede451
手塚元貴 <tezuka@genMac.local>
2024-05-14 21:23:57 +0900

    dir

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,14 @@
+
+
+// Blogに関するコンポーネントを呼び出すコンポーネント
+const Blog = () => {
+  return (
+    <div>
+      ブログ一覧
+      {/* <BlogImage></BlogImage> */}
+      {/* <BlogList></BlogList> */}
+    </div>
+  )
+}
+
+export default Blog
